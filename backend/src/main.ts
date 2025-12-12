e63f75b040ecdd1f7322e38f715b302c210f528d
vgmil <vgmiltonisaac@gmail.com>
2025-12-12 17:34:18 -0500

    🐛 fix(backend): Habilita CORS mediante middleware explícito

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors()); // Use explicit cors middleware
+  // app.enableCors({...}); // Disable built-in Nest cors to avoid conflicts if using middleware
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
